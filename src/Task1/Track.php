df087b81b508be87525bc074e4e538769cf01c42
Maxim Frolov <frolov.m.a.1991@gmail.com>
2020-05-10 10:56:28 +0300

    Implemantation Task1

--- src/Task1/Track.php
+++ src/Task1/Track.php
@@ -6,33 +6,61 @@ namespace App\Task1;
 
 class Track
 {
+    private $lapLength;
+    private $lapsNumber;
+    private $cars = [];
+
     public function __construct(float $lapLength, int $lapsNumber)
     {
-        //@todo
+        $this->lapLength = $lapLength;
+        $this->lapsNumber = $lapsNumber;
     }
 
     public function getLapLength(): float
     {
-        // @todo
+        return $this->lapLength;
     }
 
     public function getLapsNumber(): int
     {
-        // @todo
+        return $this->lapsNumber;
     }
 
     public function add(Car $car): void
     {
-        // @todo
+        $this->cars[] = $car;
     }
 
     public function all(): array
     {
-        // @todo
+        return $this->cars;
     }
 
     public function run(): Car
     {
-        // @todo
+        $cars = $this->all();
+        $time = [];
+        foreach ($cars as $z => $car)
+        {
+            $id = $car->getId();
+            $image = $car->getImage();
+            $name = $car->getName();
+            $speed = $car->getSpeed();
+            $pitStopTime = $car->getPitStopTime();
+            $fuelConsumption = $car->getFuelConsumption();
+            $fuelTankVolume = $car->getFuelTankVolume();
+
+            $distance = $this->lapsNumber * $this->lapLength; //distance of all race in kilometers
+            $amountOfFuel = $distance / $fuelConsumption; //number of all need fuel for the race
+            $numberOfRefueling = ceil($amountOfFuel / $fuelTankVolume); //
+            if ($numberOfRefueling > 0) {
+                $numberOfRefueling - 1;
+            }
+            $raceTime = ($distance * 3600) / $speed + $numberOfRefueling * $pitStopTime;
+
+            $time[$z] = $raceTime;
+        }
+
+        return $cars[array_search(min($time), $time)];
     }
 }

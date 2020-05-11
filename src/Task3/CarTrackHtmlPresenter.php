02acd4e3fe38c336a1642a01e3d03f3346a021ce
Maxim Frolov <frolov.m.a.1991@gmail.com>
2020-05-11 22:39:55 +0300

    Implementation Task3

--- src/Task3/CarTrackHtmlPresenter.php
+++ src/Task3/CarTrackHtmlPresenter.php
@@ -11,6 +11,26 @@ class CarTrackHtmlPresenter
 {
     public function present(Track $track): string
     {
-        //@todo
+        $cars = $track->all();
+        $content = "<div class='container'>" . PHP_EOL . "<h1>Cars in the Race</h1>" . PHP_EOL;
+
+        foreach ($cars as $car) {
+            $content .= "<div class='car'>" . PHP_EOL;
+            $content .= "<p><b>Id: " . $car->getId() . "</b></p>" . PHP_EOL;
+            $content .= '<img src="' . $car->getImage() . '">' . PHP_EOL;
+            $content .= "<p><b>Name: " . $car->getName() . "</b></p>" . PHP_EOL;
+            $content .= "<p><b>Max speed: " . $car->getSpeed() . " km/h</b></p>" . PHP_EOL;
+            $content .= "<p><b>PitStop time: " . $car->getPitStopTime() . " c</b></p>" . PHP_EOL;
+            $content .= "<p><b>Fuel consumption: " . $car->getFuelConsumption() . "l/100km</b></p>" . PHP_EOL;
+            $content .= "<p><b>Fuel tank volume: " . $car->getFuelTankVolume() . "</b></p>" . PHP_EOL;
+            $content .= "</div>" . PHP_EOL;
+        }
+        $content .= "</div>";
+
+        if (empty($cars)) {
+            $content = "<h1>Track is empty!</h1>";
+        }
+
+        return $content;
     }
 }
